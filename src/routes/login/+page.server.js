cd7eb5173fe010a394cc44b6bb73fead15c44e70
suresh <sureshpradhana19@gmail.com>
2024-01-24 21:02:49 +0530

    register

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,46 +1,41 @@
 import { redirect } from '@sveltejs/kit';
-import {supabase} from '$lib/server/supabase';
+import { supabase } from '$lib/server/supabase';
 
 
 export const actions = {
 	login: async (event) => {
-		const  {request,cookies}=event;
+		const { request, cookies } = event;
 		// TODO log the user in
 		let formData = await request.formData();
 		let email = formData.get('email') || '';
 		let password = formData.get('password') || '';
 
 		const { data, error } = await event.locals.supabase.auth.signInWithPassword({ email: email.toString(), password: password.toString() });
-
-		if (data) {
-			cookies.set('supabase.auth.token', data.session?.access_token ?? '', {
-				path: '/'
-			});
-			throw redirect(303, '/');
-		}else{
-			
+		if (error) {
 			throw redirect(303, '/login');
+		}
 
+
+		cookies.set('supabase.auth.token', data.session?.access_token ?? '', {
+			path: '/'
+		});
+		throw redirect(303, '/');
+
+	},
+	register: async (event) => {
+		// TODO register the user
+
+		const { request, cookies } = event;
+
+		let formData = await request.formData();
+		let email = formData.get('email') || '';
+		let password = formData.get('password') || '';
+
+		let { data, error } = await supabase.auth.signUp({ email: email.toString(), password: password.toString() })
+		if (error) {
+			throw redirect(303, '/login');
 		}
 		
-
+		throw redirect(303, '/login');
 	}
-	// register: async ({ request }) => {
-	// 	// TODO register the user
-	// 	let formData = await request.formData();
-	// 	let email = formData.get('email') || '';
-	// 	let password = formData.get('password') || '';
-	// 	let username = formData.get('username') || '';
-
-	// 	let boolean = await db.signUp(email.toString(), password.toString());
-	// 	if (boolean) {
-	// 		return {
-	// 			status: 200,
-	// 			body: {
-	// 				message: 'Registered!'
-	// 			}
-	// 		};
-
-	// 	}
-	// }
 };
